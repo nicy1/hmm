b22af46719f8d4b1f81274e304f745ef48fc6d96
nicy malanda <65350797+nicy1@users.noreply.github.com>
2020-08-13 21:29:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import hmm_readFile
 filename = 'skype_packets.csv' 
 # Get HMM parameters, train_data (for training) and test_data (for decoding)
 readFile = hmm_readFile.readFile(filename)
-(compute_transition_prob,compute_emission_prob,x_train,x_test,y_train,y_test) = readFile.get_data()
+(states, symbols, trans_prob, emis_prob, train_data, test_data, targets) = readFile.get_data()
 
-model = hmm_scaled.HmmScaled(compute_transition_prob,compute_emission_prob,'init_model.json')
+model = hmm_scaled.HmmScaled(states, symbols, trans_prob, emis_prob, 'init_model.json')
 
 # Train the HMM
-model.train(x_train)
+model.train(train_data)
 
 model.check_prob()  # Check its probabilities
 model.print_hmm()   # Print it out
@@ -24,9 +24,9 @@ print('')
 
 # Find the best hidden state sequence Q for the given observation sequence - (DECODING)
 # Using the Viterbi algorithm
-(prob,states) = model.decode(x_test[0]+x_test[1]+x_test[2], show='yes')
+(prob,states) = model.decode(test_data[0]+test_data[1]+test_data[2], show='yes')
 print('Test Decoding:')
-print('    Input:', x_test[0]+x_test[1]+x_test[2])
+print('    Input:', test_data[0]+test_data[1]+test_data[2])
 print('    Output:', states)
 print('')
 
@@ -37,16 +37,16 @@ print('')
 count = 0
 success_prediction = 0
 
-for t,obs in enumerate(x_test):
+for t,obs in enumerate(test_data):
   # Find the corresponding states which generate obs_sequence
   (prob, state) = model.decode(obs, show='no')
-  if t < len(x_test)-1:
+  if t < len(test_data)-1:
      next_state = model.predict(t, curr_state=state)
     
-     if next_state == y_test[t+1]:
+     if next_state == targets[t+1]:
         success_prediction += 1
      count += 1
-     print('time %d: predicted %s - real %s' % (t+1, next_state, y_test[t+1]))
+     print('time %d: predicted %s - real %s' % (t+1, next_state, targets[t+1]))
 
 
 print('')
