d923a3cbd9df14275d25b0389ce86b127cd45b4f
nicy malanda <65350797+nicy1@users.noreply.github.com>
2020-10-16 17:35:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,34 @@
-import hmm_scaled
-import hmm_readFile
-
-
+import hmm
+import readfile
+from sklearn.metrics import accuracy_score
 
 # ===============================================================
 
 
 filename = 'telepathology.csv' 
 # Get HMM parameters, train_data (for training) and test_data (for decoding)
-readFile = hmm_readFile.readFile(filename)
+readFile = readfile.read(filename)
 (states, symbols, trans_prob, emis_prob, train_data, test_data, targets) = readFile.get_data()
 
-model = hmm_scaled.HmmScaled(states, symbols, trans_prob, emis_prob, 'init_model.json')
-
+model = hmm.HmmScaled(states, symbols, trans_prob, emis_prob,'init_model.json')
+model.print_hmm()
 # Train the HMM
 model.train(train_data)
 
 model.check_prob()  # Check its probabilities
-model.print_hmm()   # Print it out
+#model.print_hmm()   # Print it out
 print('')
+ 
 
   # -------------------------------------------------------------
 
 # Find the best hidden state sequence Q for the given observation sequence - (DECODING)
-# Using the Viterbi algorithm
-(prob,states) = model.decode(test_data[0]+test_data[1]+test_data[2])
-print('Test Decoding:')
-print('    Input:', test_data[0]+test_data[1]+test_data[2])
-print('    Output:', states)
-print('')
-
-  # -------------------------------------------------------------
-
 # Given an observation at time t, predict the next state at t+1
-# Using Trellis diagram
-count = 0
-success_prediction = 0
+# Method: Viterbi algorithm 
 
-for t,obs in enumerate(test_data):
-  # Find the corresponding states which generate obs_sequence
-  (prob, state) = model.decode(obs, verbose=False)
-  if t < len(test_data)-1:
-     next_state = model.predict(t, curr_state=state)
-    
-     if next_state == targets[t+1]:
-        success_prediction += 1
-     count += 1
-     print('time %d: predicted %s - real %s' % (t+1, next_state, targets[t+1]))
-
-
-print('')
-prediction_accuracy = (success_prediction/count) * 100
-print('PREDICTION ACCURACY: %.2f' % (prediction_accuracy), '%')
-print('')
-
+p, y_pred = model.predict(test_data, verbose=False)
+accuracy = accuracy_score(targets, y_pred)
+print ("Accuracy: %.2f" % (accuracy*100), '%')
 
 
 # ===============================================================
