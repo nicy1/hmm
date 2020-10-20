1df7c28a2f86678ed3db283e5adae5c53f44559f
nicy malanda <65350797+nicy1@users.noreply.github.com>
2020-10-20 13:16:31 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import hmm
+import readfile
+from sklearn.metrics import accuracy_score
+
+# ===============================================================
+
+
+filename = 'telepathology.csv' 
+# Get HMM parameters, train_data (for training) and test_data (for decoding)
+readFile = readfile.read(filename)
+(states, symbols, trans_prob, emis_prob, train_data, test_data, targets) = readFile.get_data()
+
+model = hmm.HmmScaled(states, symbols, trans_prob, emis_prob,'init_model.json')
+model.print_hmm()
+# Train the HMM
+model.train(train_data)
+
+model.check_prob()  # Check its probabilities
+#model.print_hmm()   # Print it out
+print('')
+ 
+
+  # -------------------------------------------------------------
+
+# Find the best hidden state sequence Q for the given observation sequence - (DECODING)
+# Given an observation at time t, predict the next state at t+1
+# Method: Viterbi algorithm 
+
+p, y_pred = model.predict(test_data, verbose=False)
+accuracy = accuracy_score(targets, y_pred)
+print ("Accuracy: %.2f" % (accuracy*100), '%')
+
+
+# ===============================================================
+
+
+
+
+
