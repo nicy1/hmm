006e33a1e93f701e2795f2ebdc56740e60246fee
nicy malanda <65350797+nicy1@users.noreply.github.com>
2020-10-24 18:36:02 +0200

    Add files via upload

--- Benchmark/arima/main.py
+++ Benchmark/arima/main.py
@@ -1,13 +1,13 @@
 import readfile as rf
 
-from matplotlib import pyplot
 from pandas import read_csv
 from statsmodels.tsa.arima_model import ARIMA
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.metrics import accuracy_score
 
 # ==========================================================================
 
-index_table = {0.0:'2', 1.0:'4', 2.0:'5'}
+index_table = {0.0:'0', 1.0:'1', 2.0:'2', 3.0:'4', 4.0:'5'}
 count = 0
 
 def proper_round(num, dec=0):
@@ -16,7 +16,7 @@ def proper_round(num, dec=0):
     return float(num[:-2-(not dec)]+str(int(num[-2-(not dec)])+1))
   return float(num[:-1])
 
-reader = rf.reader('telepathology.csv')
+reader = rf.reader('telepathology2.csv')
 X = reader.read_csv()
 size = int(len(X) * 0.8)
 train, test = X[0:size], X[size:len(X)]
@@ -31,14 +31,13 @@ for t in range(len(test)):
    obs = test[t]
    history.append(obs)
    
-   if index_table[yhat] == index_table[obs]:
-     count += 1
-   print('predicted=%s, expected=%s' % (index_table[yhat], index_table[obs]))
-error = mean_squared_error(test, predictions)
-print('Test MSE: %.3f' % error)
-
-acc = (count/len(test)) * 100
-print('accuracy = %.2f' % acc, '%')
+accuracy = accuracy_score(test, predictions)
+precision, recall, fscore, support = score(test, predictions)
+print('accuracy = %.2f' % (accuracy*100), '%')
+print('Precision:', precision)
+print('Recall:', recall)
+print('F-Measure:', fscore)
+print('Support-score:', support)
 
 
 # ==========================================================================
--- Benchmark/arima/readfile.py
+++ Benchmark/arima/readfile.py
@@ -12,7 +12,7 @@ class reader:
         self.filename = filename
 
     def read_csv (self): 
-        table = {"2":0.0, "4":1.0, "5":2.0}  
+        table = {"0":0.0, "1":1.0, "2":2.0, "4":3.0, "5":4.0}  
         series = []
         data = csv.DictReader(open(self.filename, mode='r'))
         for row in data:
